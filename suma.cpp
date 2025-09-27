6e89eb352b4490cc162af0ac73046a36c3530cf3
Alex <aestrada1993@hotmail.com>
2025-09-27 00:16:06 -0600

    Se agregó la lectura de un tercer número y una multiplicación.

--- suma.cpp
+++ suma.cpp
@@ -3,15 +3,24 @@
 
 
 int main() {
-    int num1, num2;
+    int num1, num2, suma, num3, mul;
     
     std::cout << "Ingrese el primer numero: ";
     std::cin >> num1;
     
     std::cout << "Ingrese el segundo numero: ";
     std::cin >> num2;
+
+    suma = num1 + num2;
     
-    std::cout << "La suma es: " << num1 + num2 << std::endl;
+    std::cout << "La suma es: " << suma << std::endl;
+
+    std::cout << "Ingrese el tercer numero: ";
+    std::cin >> num3;
+
+    mul = suma * num3;
+
+    std::cout << "La suma multiplicada por el tercer numero es: " << mul << std::endl;
 
     // Pausa para que el resultado se mantenga visible
     std::cout << "Presione Enter para salir..." << std::endl;
